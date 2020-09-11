4ba693e138e4ae4c464a99f49266679dd496f01f
EternalSnowman <m3zhang@ucsd.edu>
2020-09-11 11:04:33 -0700

    added frog enemy

---
+++ Assets/Scripts/BuffDebuff/WetBuff.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WetBuff : BuffDebuff
+{
+    public float SPDValueTaken;
+    // Start is called before the first frame update
+    void Start()
+    {
+        duration = 5f;
+        currDuration = duration;
+    }
+
+    public override void Activate()
+    {
+        base.Activate();
+        SPDValueTaken = PlayerMovement.tempSpeed * 0.3f;
+        PlayerMovement.tempSpeed -= SPDValueTaken;
+    }
+
+    public override void Deactivate()
+    {
+        base.Deactivate();
+
+        PlayerMovement.tempSpeed += SPDValueTaken;
+    }
+}
