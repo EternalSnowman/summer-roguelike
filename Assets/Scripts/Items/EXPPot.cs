77b34afccf4179c007ccff0c89ed0a6adff10b2c
EternalSnowman <m3zhang@ucsd.edu>
2020-09-23 12:25:44 -0700

    small balance changes

---
+++ Assets/Scripts/Items/EXPPot.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EXPPot : Items
+{
+    public int expPlus;
+    private void Start()
+    {
+        name = "Health Potion";
+        itemDesc = "Restore 25% HP";
+    }
+
+    public override void Use()
+    {
+        PlayerStats.EXP += expPlus;
+
+        Inventory.equipConsume = Inventory.emptyItem;
+    }
+}
